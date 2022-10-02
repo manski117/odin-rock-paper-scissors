abe0600483df40e53cf6782306bc79b9b1ea8ab2
krmanski <choromanski117@gmail.com>
2022-10-02 12:18:12 -0400

    Complete if-statements and counter
    All three if-statements and switch board complete. Added 2 global
    variables to act as score-counters. Now each round adds a ++ to
    either the human or cpu score.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-
+//initialize an empty score var to be used later to count victories/losses. 
+var humanScore = 0;
+var cpuScore = 0;
 
 function getComputerChoice(){
     //this randomly returns either 'rock' 'paper' or 'scissors'
@@ -14,31 +16,62 @@ function RPSround(playerSelection, computerSelection){
     let playerMove = playerSelection.toUpperCase();
     let computerMove = computerSelection.toUpperCase();
     console.log(playerMove, computerMove);
+    let message2player
+    //This switch statement will take the function to 1 of 3 if-statements to evaluate the results of combat
     switch(playerMove){
         case "ROCK":
             console.log(`You slected ${playerMove}`)
             console.log(`and CPU selected ${computerMove}`)
             if (computerMove == "ROCK"){
-                console.log("DRAW. Nobody wins.")
-                console.log(`${playerMove} ties with ${computerMove}`)
+                message2player = `Draw. Nobody wins. ${playerMove} ties with ${computerMove}`;
+                return message2player;
             } else if (computerMove == "SCISSORS"){
-                console.log("YOU WIN!!!!!!!!!!!!!!!")
-                console.log(`${playerMove} beats ${computerMove}`)
+                message2player = `YOU WIN!!!!!!!!!! ${playerMove} beats ${computerMove}`;
+                humanScore ++;
+                return message2player;
 
             } else if (computerMove == "PAPER"){
-                console.log("You LOOSE!!!!!!!!!!!!!!!!")
-                console.log(`${computerMove} covers up ${playerMove}`)
+                message2player = `YOU LOSE!!!!!! ${computerMove} covers up ${playerMove}`;
+                cpuScore ++;
+                return message2player;
             }
             break;
         
         case "PAPER":
             console.log(`You slected ${playerMove}`)
             console.log(`and CPU selected ${computerMove}`)
+            if (computerMove == "PAPER"){
+                message2player = `Draw. Nobody wins. ${playerMove} ties with ${computerMove}`;
+                return message2player;
+            } else if (computerMove == "ROCK"){
+                message2player = `YOU WIN!!!!!!!!!! ${playerMove} covers up the ${computerMove}`;
+                humanScore ++;
+                return message2player;
+
+            } else if (computerMove == "SCISSORS"){
+                message2player = `YOU LOSE!!!!!! ${computerMove} cuts right through ${playerMove}`;
+                cpuScore ++;
+                return message2player;
+            }
+            break;
             break;
 
         case "SCISSORS":
             console.log(`You slected ${playerMove}`)
             console.log(`and CPU selected ${computerMove}`)
+            if (computerMove == "SCISSORS"){
+                message2player = `Draw. Nobody wins. ${playerMove} ties with ${computerMove}`;
+                return message2player;
+            } else if (computerMove == "PAPER"){
+                message2player = `YOU WIN!!!!!!!!!! ${playerMove} cut up the ${computerMove}`;
+                humanScore ++;
+                return message2player;
+
+            } else if (computerMove == "ROCK"){
+                message2player = `YOU LOSE!!!!!! ${computerMove} crushes the ${playerMove}`;
+                cpuScore ++;
+                return message2player;
+            }
             break;
     }
     console.log('code still continues to run past switch block TRUE')
