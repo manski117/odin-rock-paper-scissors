29a2af99c8a6a35b5c601aacb774ab42758512c3
krmanski <choromanski117@gmail.com>
2022-10-02 11:19:50 -0400

    Complete first of 3 conditionals
    There are three large if-statements I need to write for each
    possible player selection. I finished the one for if player
    selects "ROCK", and it is working well. Now I must write the
    other 2 and check to make sure they are changed around for
    those other options.

--- main.js
+++ main.js
@@ -11,7 +11,36 @@ function getComputerChoice(){
 function RPSround(playerSelection, computerSelection){
     //This function plays a single round of RPS and returns a string that declares the winner of the round
     //This function is case-insensitive, so you can type any combo of capital and lowercase
-    let playerMove = playerSelection
-    let computerMove = computerSelection;
+    let playerMove = playerSelection.toUpperCase();
+    let computerMove = computerSelection.toUpperCase();
     console.log(playerMove, computerMove);
+    switch(playerMove){
+        case "ROCK":
+            console.log(`You slected ${playerMove}`)
+            console.log(`and CPU selected ${computerMove}`)
+            if (computerMove == "ROCK"){
+                console.log("DRAW. Nobody wins.")
+                console.log(`${playerMove} ties with ${computerMove}`)
+            } else if (computerMove == "SCISSORS"){
+                console.log("YOU WIN!!!!!!!!!!!!!!!")
+                console.log(`${playerMove} beats ${computerMove}`)
+
+            } else if (computerMove == "PAPER"){
+                console.log("You LOOSE!!!!!!!!!!!!!!!!")
+                console.log(`${computerMove} covers up ${playerMove}`)
+            }
+            break;
+        
+        case "PAPER":
+            console.log(`You slected ${playerMove}`)
+            console.log(`and CPU selected ${computerMove}`)
+            break;
+
+        case "SCISSORS":
+            console.log(`You slected ${playerMove}`)
+            console.log(`and CPU selected ${computerMove}`)
+            break;
+    }
+    console.log('code still continues to run past switch block TRUE')
+
 }
