60ae00a056c227ee53b9a3f2f85e495262df2e1c
krmanski <choromanski117@gmail.com>
2022-10-02 10:52:07 -0400

    RPSround takes 2 parameters

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ function getComputerChoice(){
     //this randomly returns either 'rock' 'paper' or 'scissors'
     const choices = ["ROCK", "PAPER", "SCISSORS"];
     let randomNo = Math.floor(Math.random() * choices.length);
-    console.log(randomNo, choices[randomNo])
+    return choices[randomNo]
+}
+
+function RPSround(playerSelection, computerSelection){
+    //This function plays a single round of RPS and returns a string that declares the winner of the round
+    //This function is case-insensitive, so you can type any combo of capital and lowercase
+    let playerMove = playerSelection
+    let computerMove = computerSelection;
+    console.log(playerMove, computerMove);
 }
