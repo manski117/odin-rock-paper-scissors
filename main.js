49d737a9ed6552f2004989c540cb97ef22a3cf17
krmanski <choromanski117@gmail.com>
2022-10-06 22:04:37 -0400

    Deleted old alert messages

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var humanScore = 0;
 var cpuScore = 0; 
 const victoryCondition = 5;
 
+//Make sure score will update on webpage
+document.getElementById("h-score").innerText = humanScore;
+document.getElementById("cpu-score").innerText = cpuScore;
+
 function getComputerChoice(){
     //this randomly returns either 'rock' 'paper' or 'scissors'
     const choices = ["ROCK", "PAPER", "SCISSORS"];
@@ -11,35 +15,6 @@ function getComputerChoice(){
     return choices[randomNo]
 }
 
-function game(){
-    //use a loop to play a 5-round game that keeps score and announces the winner. 
-    alert("DO YOU WANT TO PLAY A GAME \nYou will now play a 5-round game against the computer. \nIf you win, you get to live.")
-    for (let i = 0; i < 3; i++){
-        let  choice = prompt("Type your choice of either rock, paper, or scissors").toUpperCase();
-        if ((choice == "ROCK") || (choice == "PAPER") || (choice == "SCISSORS")){
-            let message = RPSround(choice, getComputerChoice());
-            console.log(message);
-            console.log(`The score is \nHuman: ${humanScore} CPU: ${cpuScore}`);
-        } else{
-            console.log("Not an option. You broke the game. Restart")
-            //This if-statement block is to make sure the user passes in a string that works with the game.
-            break;
-        }
-        // let message = RPSround(choice, getComputerChoice());
-        // console.log(message);
-        // console.log(`The score is \nHuman: ${humanScore} CPU: ${cpuScore}`);
-    }
-    console.log(`    The FINAL score is \n    Human: ${humanScore} CPU: ${cpuScore}`)
-    if (humanScore > cpuScore){
-        console.log("    Human WINS");
-
-    } else if(humanScore < cpuScore){
-        console.log("    Machine WINS");
-
-    } else {
-        console.log("    Let's call it a draw")
-    }
-}
 
 function RPSround(playerSelection, computerSelection){
     //This function plays a single round of RPS and returns a string that declares the winner of the round
@@ -64,8 +39,7 @@ function RPSround(playerSelection, computerSelection){
                 \n You win this round. ${playerMove} crushes ${computerMove}`;
                 humanScore ++;
                 updateScoreboard();
-                
-                return message2player;
+
 
             } else if (computerMove == "PAPER"){
                 document.getElementById("log").innerText = 
@@ -74,25 +48,23 @@ function RPSround(playerSelection, computerSelection){
                 cpuScore ++;
                 updateScoreboard();
                 
-                return message2player;
             }
             break;
         
         case "PAPER":
-            alert("paper selected")
             if (computerMove == "PAPER"){
                 document.getElementById("log").innerText = 
                 `You slected ${playerMove} and CPU selected ${computerMove}.
                 \n It's a draw. Nobody wins. ${playerMove} ties with ${computerMove}`;
  
-                return message2player;
+                
             } else if (computerMove == "ROCK"){
                 document.getElementById("log").innerText = 
                 `You slected ${playerMove} and CPU selected ${computerMove}.
                 \n You win this round. ${playerMove} covers ${computerMove}`;
                 humanScore ++;
                 updateScoreboard();
-                return message2player;
+                
 
             } else if (computerMove == "SCISSORS"){
                 document.getElementById("log").innerText = 
@@ -100,27 +72,23 @@ function RPSround(playerSelection, computerSelection){
                 \n You lose this round. ${playerMove} cuts ${computerMove}`;
                 cpuScore ++;
                 updateScoreboard();
-                return message2player;
+                
             }
             break;
             break;
 
         case "SCISSORS":
-            alert("scissors selected");
             if (computerMove == "SCISSORS"){
                 document.getElementById("log").innerText = 
                 `You slected ${playerMove} and CPU selected ${computerMove}.
                 \n It's a draw. Nobody wins. ${playerMove} ties with ${computerMove}`;
 
-                return message2player;
             } else if (computerMove == "PAPER"){
                 document.getElementById("log").innerText = 
                 `You slected ${playerMove} and CPU selected ${computerMove}.
                 \n You win this round. ${playerMove} cuts ${computerMove}`;
                 humanScore ++;
                 updateScoreboard();
-                
-                return message2player;
 
             } else if (computerMove == "ROCK"){
                 document.getElementById("log").innerText = 
@@ -128,25 +96,18 @@ function RPSround(playerSelection, computerSelection){
                 \n You lose this round. ${playerMove} is crushed by ${computerMove}`;
                 cpuScore ++;
                 updateScoreboard();
-                return message2player;
             }
             break;
     }
-    console.log('code still continues to run past switch block TRUE')
+    
 
 }
 
-//Make sure score will update on webpage
-document.getElementById("h-score").innerText = humanScore;
-document.getElementById("cpu-score").innerText = cpuScore;
-
-
 
 //This code will allow for player button selection to be sent to game function
-let rockButton = document.querySelector(".rock-butt")
-rockButton.addEventListener("click", rockPress)
+let rockButton = document.querySelector(".rock-butt");
+rockButton.addEventListener("click", rockPress);
 function rockPress(){
-    alert("You pressed the rock button");
     document.getElementById("log").innerText = "rock button pressed";
     RPSround("ROCK", getComputerChoice());
     
@@ -155,7 +116,6 @@ function rockPress(){
 let paperButton = document.querySelector(".paper-butt");
 paperButton.addEventListener("click", paperPress);
 function paperPress(){
-    alert("You pressed the paper button");
     document.getElementById("log").innerText = "paper button pressed";
     RPSround("PAPER", getComputerChoice());
 }
@@ -163,27 +123,12 @@ function paperPress(){
 let scissorsButton = document.querySelector(".scissors-butt");
 scissorsButton.addEventListener("click", scissorPress);
 function scissorPress(){
-    alert("you pressed the scissors button");
     document.getElementById("log").innerText = "scissors button pressed";
-
     RPSround("SCISSORS", getComputerChoice());
 }
 
 
-
-
-
-//this will tell me if a button is pressed at all
-let button = document.querySelectorAll(".button");
-button.forEach(button => button.addEventListener("click", testButton));
-
-function testButton(){
-    alert("This button works");
-}
-
-
-
-//Run this line to change one of the scores
+//This will update the scoreboard and check victory condition
 
 function updateScoreboard(){
     document.getElementById("h-score").innerText = humanScore;
@@ -195,21 +140,30 @@ function updateScoreboard(){
 function checkWinner(){
     if ((humanScore < victoryCondition) && (cpuScore < victoryCondition)) return;
     if ((humanScore >= victoryCondition) || (cpuScore = victoryCondition)){
-        alert(`    The FINAL score is \n    Human: ${humanScore} CPU: ${cpuScore}`)
+        alert(`The FINAL score is \n    Human: ${humanScore} CPU: ${cpuScore}`)
         if (humanScore > cpuScore){
             alert("    Human WINS");
             document.getElementById("final-results-readout").innerText = "HUMAN WINS"
 
         } else if(humanScore < cpuScore){
-            alert("    Machine WINS");
+            alert("Machine WINS");
             document.getElementById("final-results-readout").innerText = "MACHINE WINS"
 
         } else {
-            alert("    Let's call it a draw")
+            alert("Let's call it a draw")
             document.getElementById("final-results-readout").innerText = "DRAW???"
         }
     }
 }
+
+// //this will tell me if a button is pressed at all
+// let button = document.querySelectorAll(".button");
+// button.forEach(button => button.addEventListener("click", testButton));
+
+// function testButton(){
+//     alert("This button works");
+// }
+
 
 
     
